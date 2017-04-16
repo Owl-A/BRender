7cd7cd2908ec83d173186ed02e14b8d119b6e6cd
Owl-A <chinubeta0710@gmail.com>
2017-04-16 07:13:32 +0530

    Camera fixed screen added

--- render.rkt
+++ render.rkt
@@ -1,9 +1,32 @@
 #lang racket
 
-(require "vectorLib.rkt" "matrixLib.rkt" "primitives.rkt" racket/draw)
+#lang racket
 
-;(define (render Scene cam)
+(require "vectorLib.rkt" "matrixLib.rkt" "primitives.rkt" racket/draw)
+(struct color (red green blue) #:transparent )
+
+;(define (render Scene cam shaders Scr)
+;  
 ;  )
+
+(define Screen%
+  (class object%
+    (init l b)
+    (define buffer (make-object bitmap% l b))
+    (super-new)
+    (define/public (paint x y Clr)
+      (send buffer set-argb-pixels x   ; x
+            y              ; y
+            1              ; width
+            1              ; height
+            (list->bytes 
+             (list 0                              ; alpha
+                   (color-red Clr)                ; red
+                   (color-green Clr)              ; green 
+                   (color-blue Clr))))            ; blue
+      )
+    (define/public (save-buffer)
+      (send buffer save-file "image.png" 'png))))
 
 (define camera%
   (class object%
@@ -15,7 +38,7 @@
     [define aspect (/ image-wid image-hgt)]
     ; (0,0) for the canvas, wise men said keep it at top left
 
-    (define ctr-x 0)
+    (define ctr-x -1)
     (define ctr-y 0)
     [field
        [temp (subs to from)]
@@ -23,29 +46,30 @@
        [z_ (normalise leng2 temp)]
        [y_ (make-orthonormal-to z_ temp_y)]
        [x_ (cross y_ z_)]]
-    (define del-x (scale (/ aspect image-hgt) x_)); delta-x
-    (define del-y (scale (/ -1 image-hgt) y_)); delta-y
-    (define ret-x (subs (scale (- 0 aspect) x_) del-x))
-    (define ret-y (subs (scale -1 y_) del-y))
-    (field [base (subs (subs (add z_ (add (scale 0.5 y_) (scale (* -0.5 aspect) x_)))
-                      (scale 0.5 del-x)) (scale 0.5 del-y))])
+    (define del-x (scale (/ aspect image-wid) x_)); delta-x
+    (define del-y (scale (/ -1 image-hgt) y_)); delta-y     
+    (define ret-x (add (scale (- 0 aspect) x_) del-x))
+    (define ret-y (add y_ del-y) )
+    (define img-hgt-1 (- image-hgt 1))
+    (define img-wid-1 (- image-wid 1))
+    (field [base  (add (subs z_ (add (scale (* 0.5 aspect) x_ )(scale 0.5 del-x)))
+                              (scale 0.5 (add y_ del-y)))])
     [super-new]
-    (define/public (get-next-ray)
+    (define/public (get-next-ray)  ; getting the next ray
       (begin
-        ;return ray :- to be added
-        (cond ((< ctr-x image-wid) (begin
+        (cond ((< ctr-x img-wid-1) (begin
                                      (set! ctr-x (+ ctr-x 1))
                                      (set! base (add base del-x))
-                                     base))
-              ((< ctr-y image-hgt) (begin (set! ctr-x 0)
+                                     (make-ray from (subs base from))))
+              ((< ctr-y img-hgt-1) (begin (set! ctr-x 0)
                                           (set! ctr-y (+ ctr-y 1))
-                                          (set! base (add base (add ret-x del-y)))
-                                          base
+                                          (set! base (add (add base ret-x) del-y))
+                                          (make-ray from (subs base from))
                                           ))
-              (else (begin (set! ctr-x 0)
+              (else (begin (set! ctr-x -1)
                            (set! ctr-y 0)
-                           (set! base (add base (add ret-x ret-y)))
-                           'done)))));keep track of position
-    ))
+                           (set! base (add base (add
+                                                 (subs ret-x del-x) ret-y)))
+                           'done))));keep track of position
+    ))); apparently superfluous state variables are being added as a memory tradeoff for less time complexity
 
-;; TODO : error the center of the rays is not 1,0,0 BALANCE IT!
